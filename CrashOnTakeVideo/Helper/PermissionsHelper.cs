64eb3788b45c97ddf87e2d4979bb9b59076e3a3d
Mihai Vasile <mihaiteodorv@gmail.com>
2025-04-09 16:40:41 +0300

    [Feature] Reproductible repo

---
+++ CrashOnTakeVideo/Helper/PermissionsHelper.cs
@@ -0,0 +1,33 @@
+﻿using static Microsoft.Maui.ApplicationModel.Permissions;
+
+namespace CrashOnTakeVideo.Helper
+{
+    public static class PermissionsHelper
+    {
+        public static async Task<PermissionStatus> CheckAndRequestPermission<T>() where T : BasePermission, new()
+        {
+            PermissionStatus status = await Permissions.CheckStatusAsync<T>();
+
+            if (status == PermissionStatus.Granted)
+                return status;
+
+            if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                return status;
+            }
+
+            if (Permissions.ShouldShowRationale<T>())
+            {
+                //skip
+            }
+
+            status = await Permissions.RequestAsync<T>();
+
+            if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.Android)
+            {
+            }
+
+            return status;
+        }
+    }
+}
---
+++ CrashOnTakeVideo/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using CrashOnTakeVideo.Helper;
+
+namespace CrashOnTakeVideo
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            var devicePlatform = DeviceInfo.Current.Platform;
+            var deviceVersion = DeviceInfo.Current.Version;
+
+            if (devicePlatform == DevicePlatform.Android && deviceVersion.Major <= 12)
+            {
+                var storageStatus = await PermissionsHelper.CheckAndRequestPermission<Permissions.StorageWrite>();
+                if (storageStatus == PermissionStatus.Denied)
+                {
+                    return;
+                }
+            }
+
+            var status = await PermissionsHelper.CheckAndRequestPermission<Permissions.Camera>();
+            if (status == PermissionStatus.Denied || status == PermissionStatus.Unknown)
+            {
+                return;
+            }
+
+            var result = await MediaPicker.Default.CaptureVideoAsync();
+            //upload
+            if (result == null)
+                return;
+        }
+    }
+
+}
